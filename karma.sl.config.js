2d81eb0d1e83ade5217a1b1e2bf9881a2b1521e7
Pawel Psztyc <jarrodek@gmail.com>
2020-01-28 11:55:17 -0800

    test: updating SL config

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -1,14 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('deepmerge');
-const slSettings = require('@advanced-rest-client/testing-karma-sl/sl-settings.js');
+const { slSettings } = require('@advanced-rest-client/testing-karma-sl');
 const createBaseConfig = require('./karma.conf.js');
 
 module.exports = (config) => {
   const slConfig = merge(slSettings(config), {
     sauceLabs: {
-      testName: 'equest-editor',
+      testName: 'request-editor',
     },
   });
+  // if you want to change default browsers
   slConfig.browsers = [
     'SL_Chrome',
     'SL_Chrome-1',
@@ -16,7 +17,8 @@ module.exports = (config) => {
     'SL_Firefox-1',
     'SL_Safari',
     'SL_Safari-1',
-    // 'SL_EDGE',
+    'SL_EDGE',
+    // 'SL_EDGE-1',
   ];
   config.set(merge(createBaseConfig(config), slConfig));
   return config;
